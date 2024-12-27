fe76b5c12242ea68ba883dc9bd8a303a787d63c4
Yao, Achi <achi.noel@hotmail.com>
2024-12-27 22:48:05 +0100

    chore: add benchmark script

--- demos/server/main.cpp
+++ demos/server/main.cpp
@@ -5,11 +5,12 @@
 #include <spdlog/spdlog.h>
 #include "tcp_server.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     spdlog::set_level(spdlog::level::info);
+    std::string ip = argv[1];
     const size_t max_io_workers = std::jthread::hardware_concurrency() - 2;
     try {
-        TcpServer server("127.0.0.1", 8080, 2048, max_io_workers);
+        TcpServer server(ip, 8080, 2048, max_io_workers);
         server.run();
     } catch (const std::exception &ex) {
         std::cerr << "Error: " << ex.what() << "\n";
