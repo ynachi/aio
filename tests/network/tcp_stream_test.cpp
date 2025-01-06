fe1a3596d9dad5e47d9df88b38517a6b9d6bafe0
Yao, Achi <achi.noel@hotmail.com>
2025-01-06 22:31:25 +0100

    chore: add in-memory stream for testing

---
+++ tests/network/tcp_stream_test.cpp
@@ -0,0 +1,57 @@
+#include "network/tcp_stream.h"
+
+#include <async_simple/coro/SyncAwait.h>
+#include <gtest/gtest.h>
+#include <spdlog/common.h>
+
+#include "io_context/memory_stream_context.h"
+
+class TcpStreamTest : public ::testing::Test
+{
+protected:
+    TcpStreamTest() : tcp_stream_(5, std::make_shared<MemoryStreamContext>(), "", "") {}
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+    TcpStream tcp_stream_;
+};
+
+TEST(BufferTest, SizeReturnsActualDataSize)
+{
+    Buffer buf(100);
+    EXPECT_EQ(buf.size(), 0);
+    EXPECT_EQ(buf.capacity(), 100);
+
+    buf.write("hello", 5);
+    EXPECT_EQ(buf.size(), 5);
+    EXPECT_EQ(buf.capacity(), 100);
+
+    buf.consume(3);
+    EXPECT_EQ(buf.size(), 2);
+    EXPECT_EQ(buf.capacity(), 100);
+}
+
+TEST_F(TcpStreamTest, ReadWriteSuccess)
+{
+    std::string data = "Hello, World!";
+    // tcp_stream_.read_buffer().write(data.data(), data.size());
+    auto write_res = async_simple::coro::syncAwait(tcp_stream_.write(data));
+
+    ASSERT_TRUE(write_res.has_value());
+
+    // std::vector<char> buffer(5);
+    // auto result = async_simple::coro::syncAwait(tcp_stream_.read(buffer));
+
+    // ASSERT_TRUE(result);
+    // ASSERT_EQ(result.value(), 5);
+    // ASSERT_EQ(std::string(buffer.data(), 5), "Hello");
+}
+
+int main(int argc, char** argv)
+{
+    spdlog::set_level(spdlog::level::debug);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
